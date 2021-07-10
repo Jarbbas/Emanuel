54c4f435063ba53bbb7a2d3ed32d824cb94f45a8
Emanuel Rodrigues <44001420+Jarbbas@users.noreply.github.com>
2021-07-10 15:31:16 +0100

    Second commit
    O primeiro commit tinha o código fonte errado, apenas tinha o CV este é o código fonte da página

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,23 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId),
-        nav = document.getElementById(navId)
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
-    /*===== MENU SHOW =====*/
-    /* Validate if constant exists */
-    if (toggle && nav) {
-        toggle.addEventListener('click', () => {
-            nav.classList.toggle('show-menu')
-        })
-    }
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
 }
-showMenu('nav-toggle', 'nav-menu')
 
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 
 /*==================== REMOVE MENU MOBILE ====================*/
@@ -25,7 +30,105 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
+/*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header')
+
+      function toggleSkills(){
+          let itemClass = this.parentNode.className
+
+          for (i = 0; i < skillsContent.length; i++) {
+              skillsContent[i].className = 'skills__content skills__close'
+          }
+          if(itemClass === 'skills__content skills__close'){
+              this.parentNode.className = 'skills__content skills__open'
+          }
+      }
+
+      skillsHeader.forEach((el) =>{
+          el.addEventListener('click', toggleSkills)
+      })
+/*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+      tabs.forEach(tab =>{
+          tab.addEventListener('click', () =>{
+              const target = document.querySelector(tab.dataset.target)
+
+              tabContents.forEach(tabContent =>{
+                  tabContent.classList.remove('qualification__active')
+              })
+              target.classList.add('qualification__active')
+
+              tabs.forEach(tab =>{
+                  tab.classList.remove('qualification__active')
+              })
+              tab.classList.add('qualification__active')
+          })
+      })
+
+/*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close')
+
+      let modal = function(modalClick){
+        modalViews[modalClick].classList.add('active-modal')
+      }
+
+      modalBtns.forEach((modalBtn, i) => {
+        modalBtn.addEventListener('click', () => {
+            modal(i)
+        })
+      })
+
+      modalCloses.forEach((modalClose) => {
+        modalClose.addEventListener('click', () => {
+            modalViews.forEach((modalView) =>{
+                modalView.classList.remove('active-modal')
+            })
+        })
+      })
+      
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+let swiperPortfolio = new Swiper ('.portfolio__container', {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination:{
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+/*==================== TESTIMONIAL ====================*/
+let swipertestimonial = new Swiper ('.testimonial__container', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+
+    pagination:{
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+        clickable: true,
+    },
+    breakpoints:{
+        568:{
+            slidesPerView: 2,
+        },
+        780:{
+            slidesPerView: 3,
+        },
+    }
+});
+
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive(){
@@ -44,19 +147,27 @@ function scrollActive(){
     })
 }
 window.addEventListener('scroll', scrollActive)
-
-/*==================== SHOW SCROLL TOP ====================*/
-function scrollTop(){
-    const scrollTop = document.getElementById('scroll-top');
-    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
-window.addEventListener('scroll', scrollTop)
+window.addEventListener('scroll', scrollHeader)
 
-/*==================== DARK LIGHT THEME ====================*/
+/*==================== SHOW SCROLL UP ====================*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*==================== DARK LIGHT THEME ====================*/ 
+
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
-const iconTheme = 'bx-sun'
+const iconTheme = 'uil-sun'
 
 // Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme')
@@ -64,13 +175,13 @@ const selectedIcon = localStorage.getItem('selected-icon')
 
 // We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
 }
 
 // Activate / deactivate the theme manually with the button
@@ -81,45 +192,4 @@ themeButton.addEventListener('click', () => {
     // We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
-})
-
-/*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ====================*/
-function scaleCv(){
-    document.body.classList.add('scale-cv')
-}
-
-/*==================== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ====================*/
-function removeScale(){
-    document.body.classList.remove('scale-cv')
-}
-
-/*==================== GENERATE PDF ====================*/
-// PDF generated area
-let areaCv = document.getElementById('area-cv')
-let resumeButton = document.getElementById('resume-button')
-// Html2pdf options
-let opt = {
-  margin:       1,
-  filename:     'EmanuelCv.pdf',
-  image:        { type: 'jpeg', quality: 0.98 },
-  html2canvas:  { scale: 4 },
-  jsPDF:        { format: 'a4', orientation: 'portrait' }
-};
-
-// Function to call areaCv and Html2Pdf options 
-    function generateResume(){
-        html2pdf(areaCv, opt)
-    }
-
-// When the button is clicked, it executes the three functions
-    resumeButton.addEventListener('click', () =>{
-
-    // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
-        scaleCv();
-
-    // 2. The PDF is generated
-        generateResume()
-
-    // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
-    setTimeout(removeScale, 5000)
 })
